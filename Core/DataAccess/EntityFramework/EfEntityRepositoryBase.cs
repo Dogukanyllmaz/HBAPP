0b306806956432fc4be3dc4d489bddb0c519a6a6
Dogu_ <dogu_rog@hotmail.com>
2024-09-12 05:08:09 +0300

    Entityframework entegrasyonu

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,71 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+
+            //IDisopable pattern implementation of c#
+            //işin biter bitmez garbage collectorden kaldır demek ** usıng
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+        {
+
+            using (TContext context = new TContext())
+            {
+                //filtre bos mu 
+                //evetse tumunu getir
+                //değilse verdiğim filtreyi getir
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Remove(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deleteEntity = context.Entry(entity);
+                deleteEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
