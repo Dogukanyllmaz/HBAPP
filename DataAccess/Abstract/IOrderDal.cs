16cffa44f286c4c283d164ebb7c2175a156bd1c9
Dogu_ <dogu_rog@hotmail.com>
2024-09-12 05:55:45 +0300

    Ef entegration and dtos

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -1,6 +1,6 @@
 ﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
 using Entities.Concrete;
-using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface IOrderDal:IEntityRepository<Order>
     {
-
-        List<ProductDetailDto> GetProductDetails();
-
     }
 }
